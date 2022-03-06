df37901a5d9b116942d5fcf64a68795b6345ffd0
edwinmejia-123 <edwin_adonay123@hotmail.com>
2022-03-06 01:16:11 -0600

    Estructura de proyecto MVC

---
+++ config/conexion.php
@@ -0,0 +1,20 @@
+<?php
+class Conectar{
+    private $host, $user, $pass, $database, $port;
+
+    public function __construct()
+    {
+     $this->host="localhost";
+     $this->user="root";
+     $this->pass="";
+     $this->database="tripadivisor";
+     $this->port="3306";
+    }
+
+    public function conexion(){
+        $con = new mysqli($this->host,$this->user,$this->pass,$this->database,$this->port);
+        return $con;
+    }
+}
+    
+?>
